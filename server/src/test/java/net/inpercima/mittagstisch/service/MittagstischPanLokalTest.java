32832a006590033ddecda6f3dba94ff0fb207395
inpercima <inpercima@gmail.com>
2020-01-03 11:53:41 +0100

    Activate Pan Lokal

--- server/src/test/java/net/inpercima/mittagstisch/service/MittagstischPanLokalTest.java
+++ server/src/test/java/net/inpercima/mittagstisch/service/MittagstischPanLokalTest.java
@@ -7,7 +7,6 @@ import java.io.IOException;
 
 import org.assertj.core.api.Condition;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import net.inpercima.mittagstisch.model.Lunch;
@@ -22,20 +21,20 @@ public class MittagstischPanLokalTest {
     }
 
     @Test
-    @Disabled("page does not exist")
     public void panLokal() throws IOException {
         mpl.getHtmlPage(mpl.getUrl());
-        assertThat(mpl.getHtmlPage().getTitleText()).isEqualTo("Mittagessen Archives - Pan");
+        assertThat(mpl.getHtmlPage().getTitleText()).isEqualTo("Mittagessen Archives - Pan Lokal");
         final String week = mpl.getWeek(mpl.getWeekSelector());
-        assertThat(week).is(
-                anyOf(new Condition<>(week::contains, "Wochenkarte"), new Condition<>(week::contains, "Mittagskarte")));
+        Condition<String> wochenkarte = new Condition<>(s -> s.contains("Wochenkarte"), "type Wochenkarte");
+        Condition<String> mittagskarte = new Condition<>(s -> s.contains("Mittagskarte"), "type Mittagskarte");
+        assertThat(week).is(anyOf(wochenkarte, mittagskarte));
     }
 
     @Test
-    @Disabled("page does not exist")
     public void shouldPrepare() throws IOException {
         final Lunch lunch = mpl.prepare();
         assertThat(lunch).isNotNull();
+        assertThat(lunch.getFood()).isNotEmpty();
     }
 
 }
