a29555aaf8d31770293c071c3f396b1c2a4427a8
Marcel Hendrich <inpercima@gmail.com>
2023-10-31 12:29:25 +0100

    fix(): Check pdf link if this is a pdf, otherwise load image
    fix #53

--- backend/src/main/java/net/inpercima/mittagstisch/service/BistroAmKanal.java
+++ backend/src/main/java/net/inpercima/mittagstisch/service/BistroAmKanal.java
@@ -2,9 +2,11 @@ package net.inpercima.mittagstisch.service;
 
 import org.apache.commons.lang3.StringUtils;
 
+import lombok.extern.slf4j.Slf4j;
 import net.inpercima.mittagstisch.model.Bistro;
 import net.inpercima.mittagstisch.model.Lunch;
 
+@Slf4j
 public class BistroAmKanal extends Mittagstisch {
 
     public BistroAmKanal(final int days) {
@@ -12,9 +14,9 @@ public class BistroAmKanal extends Mittagstisch {
         bistro.setPdf(true);
         bistro.setDaily(true);
         bistro.setDays(days);
-        bistro.setLunchSelector("");
+        bistro.setLunchSelector("main wow-image img");
         bistro.setName("Bistro am Kanal");
-        bistro.setUrl("https://buschmannle.wixsite.com/meinewebsite");
+        bistro.setUrl("https://buschmannle.wixsite.com/meinewebsite/wochenkarte");
         bistro.setWeekSelector("main + div a[class~='wixui-button']");
         bistro.setWeekSelectorXPath("/html/body/div/div/div[4]/div/div/div/div/section/div[2]/div/div[2]/div/div[4]/a");
         setBistro(bistro);
@@ -27,7 +29,15 @@ public class BistroAmKanal extends Mittagstisch {
      */
     public Lunch parse() {
         String mealWithDayAndPrice = StringUtils.EMPTY;
-        return buildLunch(mealWithDayAndPrice);
+        Lunch lunch = buildLunch(mealWithDayAndPrice);
+        if (getBistro().isPdf() && !lunch.getPdfLink().endsWith("pdf")) {
+            final String linkedImage = getHtmlPage().querySelector(getBistro().getLunchSelector()).getAttributes()
+                    .getNamedItem("src")
+                    .getNodeValue();
+            log.debug("prepare lunch for: '{}' with alternative image link: '{}'", getBistro().getName(), linkedImage);
+            lunch.setPdfLink(linkedImage);
+        }
+        return lunch;
     }
 
     public boolean isWithinWeek(final boolean checkForNextWeek) {
