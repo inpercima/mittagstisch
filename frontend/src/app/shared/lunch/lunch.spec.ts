5ec8707ee8e1b1eaf3dca227745d4b003f678381
Marcel Hendrich <inpercima@gmail.com>
2026-01-03 21:52:53 +0100

    fix(): fix linting

--- frontend/src/app/shared/lunch/lunch.spec.ts
+++ frontend/src/app/shared/lunch/lunch.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { ActivatedRoute, provideRouter } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 import { Lunch } from './lunch';
 
 describe('Lunch', () => {
