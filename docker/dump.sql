342e6bdb52a904e8fe270e6da3fcfd1e98616419
Marcel Hendrich <inpercima@gmail.com>
2026-02-19 22:02:33 +0100

    feat(): update docker configurations and SQL dump for improved setup

--- docker/dump.sql
+++ docker/dump.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: mysql
--- Erstellungszeit: 25. Jan 2026 um 19:32
+-- Erstellungszeit: 19. Feb 2026 um 20:41
 -- Server-Version: 8.0.44
 -- PHP-Version: 8.3.30
 
@@ -41,7 +41,9 @@ CREATE TABLE `bistro` (
 INSERT INTO `bistro` (`id`, `name`, `selector`, `url`) VALUES
 (1, 'Bistro im BIC', 'main#content_main', 'http://www.bistro-bic.de/wochenkarte'),
 (2, 'Rocky Maria - Kantine Tapetenwerk', 'main div section.info-box', 'https://rocky-maria.de/kantine'),
-(3, 'Geschmackssache Leipzig', '#main > .entry:first-of-type table', 'https://geschmackssache-leipzig.de');
+(3, 'Geschmackssache Leipzig', '#main > .entry:first-of-type table', 'https://geschmackssache-leipzig.de'),
+(4, 'BIOMARE', 'main section#mittagstisch', 'https://www.bio-mare.com/'),
+(5, 'Kirow Kantine', 'div#c21', 'https://www.kirow-kantine.de/');
 
 -- --------------------------------------------------------
 
@@ -83,13 +85,13 @@ ALTER TABLE `lunch`
 -- AUTO_INCREMENT für Tabelle `bistro`
 --
 ALTER TABLE `bistro`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT für Tabelle `lunch`
 --
 ALTER TABLE `lunch`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=55;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
 
 --
 -- Constraints der exportierten Tabellen
