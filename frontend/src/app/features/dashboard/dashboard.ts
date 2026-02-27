5f3144574bec6424c2a37512a408b9ab1369b26d
Marcel Hendrich <inpercima@gmail.com>
2026-02-27 17:18:41 +0100

    feat:() enhance dashboard with dynamic date display for routes

--- frontend/src/app/features/dashboard/dashboard.ts
+++ frontend/src/app/features/dashboard/dashboard.ts
@@ -1,3 +1,4 @@
+import { DatePipe } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet, Routes } from '@angular/router';
 import { routeConfig } from '../../app.routes';
@@ -5,8 +6,10 @@ import { routeConfig } from '../../app.routes';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.html',
-  imports: [RouterLinkActive, RouterLink, RouterOutlet],
+  imports: [RouterLinkActive, RouterLink, RouterOutlet, DatePipe],
 })
 export class Dashboard {
   protected routes: Routes = routeConfig.find((r) => r.children)?.children ?? [{ path: '', data: { label: '' } }];
+  protected today = new Date();
+  protected tomorrow = new Date(this.today.getTime() + 1 * 24 * 60 * 60 * 1000);
 }
